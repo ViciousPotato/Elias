28dd858e18a83bea49bed1ea2e4ab784be90e382
Zhou Honglin <zhou.honglin@gmail.com>
2014-03-30 14:19:28 +0800

    添加deploy脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Instal mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+apt-get install mongodb-10gen=2.2.3
+
+# Install nodejs
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make nodejs
