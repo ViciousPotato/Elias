ba3d4f0045045a25a96f7b690f716b403a66379e
ViciousPotato <Zhou.Honglin@Gmail.com>
2015-02-02 23:45:29 +0800

    Add h1 btn function to editor #32

--- static/js/application.js
+++ static/js/application.js
@@ -1,10 +1,23 @@
-// Some general UI pack related JS
-
-// I don't know what this is.
-$(function () {
-  // Custom selects
-  $("select").dropkick();
-});
+HTMLTextAreaElement.prototype.insertAtCaret = function (text) {
+  text = text || '';
+  if (document.selection) {
+    // IE
+    this.focus();
+    var sel = document.selection.createRange();
+    sel.text = text;
+  } else if (this.selectionStart || this.selectionStart === 0) {
+    // Others
+    var startPos = this.selectionStart;
+    var endPos = this.selectionEnd;
+    this.value = this.value.substring(0, startPos) +
+      text +
+      this.value.substring(endPos, this.value.length);
+    this.selectionStart = startPos + text.length;
+    this.selectionEnd = startPos + text.length;
+  } else {
+    this.value += text;
+  }
+};
 
 $(document).ready(function() {
   var gBitTmpl = Handlebars.compile($('#bit-template').html());
---
+++ static/js/edit.js
@@ -0,0 +1,3 @@
+$('#editor-h1').click(function() {
+  document.getElementById('editor').insertAtCaret('#');
+});
