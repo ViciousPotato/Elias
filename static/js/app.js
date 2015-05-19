2ce60f71988deae00f48d8f50225801427648d76
ViciousPotato <Zhou.Honglin@Gmail.com>
2015-05-20 00:42:12 +0800

    Layout bit at right

--- static/js/app.js
+++ static/js/app.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
   stackBlurImage('blur-img', 'blur-canvas', 180, false);
 
   var articleTemplate = Handlebars.compile($('#article-template').html());
+  var bitListTemplate = Handlebars.compile($('#bit-list-template').html());
 
   $.get('/topic/Random', function(res) {
     var article = articleTemplate(res);
     $(".article-content").html(article);
+
+    var bitList = bitListTemplate({bits: res.bits});
+    $(".article-right").html(bitList);
   });
 });
